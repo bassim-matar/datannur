8cb3b50920849aaf7f609493250991c2f311c8cb
Bassim <bassim.matar@gmail.com>
2024-10-16 20:19:39 +0200

    fixed : tabs_body overflow hidden

--- src/tab/TabsBody.svelte
+++ src/tab/TabsBody.svelte
@@ -112,6 +112,7 @@
     .tabs_body {
       border-radius: $rounded-bottom;
       border-top-right-radius: $rounded-size;
+      overflow: hidden;
     }
     .tabs_body.no_first_tab {
       border-top-left-radius: $rounded-size;
