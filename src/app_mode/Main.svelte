51e0624646792da5767a9567a5367de9557b49e2
Bassim <bassim.matar@gmail.com>
2024-10-18 21:39:26 +0200

    added : special error page for when error during init process

--- src/app_mode/Main.svelte
+++ src/app_mode/Main.svelte
@@ -28,6 +28,8 @@
   import { copy_text_listen_click } from "@js/copy_text"
   import default_banner from "@markdown/main/banner.md?raw"
 
+  let error_loading_db = false
+
   const timer = performance.now()
 
   function set_option_default(key, value = true) {
@@ -55,33 +57,37 @@
 
   db.search = async () => {}
   db.loaded = (async () => {
-    
-    let timer = performance.now()
-    await Main_filter.init()
-    const filter = {
-      entity: "dataset",
-      variable: "type",
-      values: Main_filter.get_type_to_filter(),
+    try {
+      let timer = performance.now()
+      await Main_filter.init()
+      const filter = {
+        entity: "dataset",
+        variable: "type",
+        values: Main_filter.get_type_to_filter(),
+      }
+      console.log("init filter", Math.round(performance.now() - timer) + " ms")
+
+      timer = performance.now()
+      await db.init({ filter })
+      console.log("load db", Math.round(performance.now() - timer) + " ms")
+
+      timer = performance.now()
+      const user_data = await get_user_data()
+      db.add_meta(user_data)
+      db_add_processed_data()
+      console.log("process db", Math.round(performance.now() - timer) + " ms")
+
+      timer = performance.now()
+      const search = new Search()
+      search.init()
+      db.search = async (...args) => await search.search(...args)
+      Logs.init(user_data.log)
+      Favorites.init(user_data.favorite)
+      SearchHistory.init(user_data.search_history, { limit: 100 })
+    } catch (e) {
+      console.error(e)
+      error_loading_db = true
     }
-    console.log("init filter", Math.round(performance.now() - timer) + " ms")
-
-    timer = performance.now()
-    await db.init({ filter })
-    console.log("load db", Math.round(performance.now() - timer) + " ms")
-
-    timer = performance.now()
-    const user_data = await get_user_data()
-    db.add_meta(user_data)
-    db_add_processed_data()
-    console.log("process db", Math.round(performance.now() - timer) + " ms")
-
-    timer = performance.now()
-    const search = new Search()
-    search.init()
-    db.search = async (...args) => await search.search(...args)
-    Logs.init(user_data.log)
-    Favorites.init(user_data.favorite)
-    SearchHistory.init(user_data.search_history, { limit: 100 })
   })()
 
   async function check_from_search(page_hash_value) {
@@ -138,7 +144,10 @@
       ? db.get_config("banner")
       : default_banner
     banner_src = banner_src?.split("(")[1]?.split(")")[0]
-    main_banner.src = banner_src?.replaceAll("{dark_mode}", is_dark ? "_dark" : "")
+    main_banner.src = banner_src?.replaceAll(
+      "{dark_mode}",
+      is_dark ? "_dark" : "",
+    )
     main_banner.onload = () => {
       const css_var_style = document.documentElement.style
       css_var_style.setProperty("--main_banner_width", main_banner.width)
@@ -159,16 +168,44 @@
 {#await Options.loaded then}
   <Header />
   <div id="wrapper" class:no_footer={!$footer_visible}>
-    {#await db.loaded}
-      <Loading />
-    {:then}
-      <Router />
-      <div id="db_loaded" style="display: none;"></div>
-      <div
-        id="page_loaded_route_{page_loaded_route}"
-        style="display: none;"
-      ></div>
-    {/await}
+    {#if error_loading_db}
+      <div class="error_loading_db">
+        <h2 class="title">Erreur de chargement</h2>
+        <p>Erreur durant le chargement de la base de données.</p>
+        <p>Veuillez réessayer de charger l'application plus tard.</p>
+        <p>Si le problème persiste, contactez le support.<p/>
+      </div>
+    {:else}
+      {#await db.loaded}
+        <Loading />
+      {:then}
+        <Router />
+        <div id="db_loaded" style="display: none;"></div>
+        <div
+          id="page_loaded_route_{page_loaded_route}"
+          style="display: none;"
+        ></div>
+      {/await}
+    {/if}
   </div>
   <Footer />
 {/await}
+
+<style lang="scss">
+  @use "../main.scss" as *;
+
+  .error_loading_db {
+    position: absolute;
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    max-width: 700px;
+    h2 {
+      color: $color-3;
+    }
+    p{
+      margin: 0.5em 0;
+    }
+  }
+</style>
